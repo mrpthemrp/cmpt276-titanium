c434ea24d10dc0d06700aff800eba856ef1bb5a3
Christopher Torunski <cjt13@sfu.ca>
2021-10-21 16:37:04 -0700

    Improve remove method performance and add log messages

--- app/src/main/java/ca/cmpt276/titanium/model/Children.java
+++ app/src/main/java/ca/cmpt276/titanium/model/Children.java
@@ -6,9 +6,12 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Children {
     private static final Children instance = new Children();
+    private static final Logger logger = Logger.getLogger(Children.class.getName());
     private static ArrayList<Child> children = new ArrayList<>();
     private static ArrayList<CoinFlip> coinFlips = new ArrayList<>();
 
@@ -33,10 +36,14 @@ public class Children {
 
         if (childrenJson != null) {
             Children.children = gson.fromJson(childrenJson, childrenType);
+        } else {
+            logger.log(Level.INFO, "No Child objects were loaded into Children.children");
         }
 
         if (coinFlipsJson != null) {
             Children.coinFlips = gson.fromJson(coinFlipsJson, coinFlipsType);
+        } else {
+            logger.log(Level.INFO, "No CoinFlip objects were loaded into Children.coinFlips");
         }
     }
 
@@ -59,13 +66,15 @@ public class Children {
 
         if (children != null && !children.isEmpty()) {
             uniqueId = children.get(children.size() - 1).getUniqueId() + 1;
+        } else {
+            logger.log(Level.INFO, "Children.children unique ID generation restarted from 0");
         }
 
         return uniqueId;
     }
 
     public void addChild(Context context, String name) {
-        Child newChild = new Child(name, generateUniqueChildId());
+        Child newChild = new Child(generateUniqueChildId(), name);
         Children.children.add(newChild);
         saveData(context);
     }
@@ -77,20 +86,23 @@ public class Children {
             }
         }
 
+        logger.log(Level.WARNING, "Attempted to get Child object with nonexistent unique ID");
         return null;
     }
 
     public void removeChild(Context context, int uniqueId) {
-        Child badChild = null;
+        Child badChildIndex = null;
 
         for (int i = 0; i < children.size(); i++) {
             if (uniqueId == children.get(i).getUniqueId()) {
-                badChild = Children.children.get(i);
+                badChildIndex = Children.children.get(i);
             }
         }
 
-        if (badChild != null) {
-            Children.children.remove(badChild);
+        if (badChildIndex != null) {
+            Children.children.remove(badChildIndex);
+        } else {
+            logger.log(Level.WARNING, "Attempted to remove Child object with nonexistent unique ID from Children.children");
         }
 
         saveData(context);
@@ -101,6 +113,8 @@ public class Children {
 
         if (coinFlips != null && !coinFlips.isEmpty()) {
             uniqueId = coinFlips.get(coinFlips.size() - 1).getUniqueId() + 1;
+        } else {
+            logger.log(Level.INFO, "Children.coinFlips unique ID generation restarted from 0");
         }
 
         return uniqueId;
@@ -119,20 +133,23 @@ public class Children {
             }
         }
 
+        logger.log(Level.WARNING, "Attempted to get CoinFlip object with nonexistent unique ID");
         return null;
     }
 
     public void removeCoinFlip(Context context, int uniqueId) {
-        CoinFlip deletedCoinFlip = null;
+        CoinFlip deletedCoinFlipIndex = null;
 
         for (int i = 0; i < coinFlips.size(); i++) {
             if (uniqueId == coinFlips.get(i).getUniqueId()) {
-                deletedCoinFlip = coinFlips.get(i);
+                deletedCoinFlipIndex = coinFlips.get(i);
             }
         }
 
-        if (deletedCoinFlip != null) {
-            Children.coinFlips.remove(deletedCoinFlip);
+        if (deletedCoinFlipIndex != null) {
+            Children.coinFlips.remove(deletedCoinFlipIndex);
+        } else {
+            logger.log(Level.WARNING, "Attempted to remove CoinFlip object with nonexistent unique ID from Children.coinFlips");
         }
 
         saveData(context);
