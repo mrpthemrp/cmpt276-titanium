f93816fa2f9073f10caa4164a6a1695a84bdbee4
Christopher Torunski <cjt13@sfu.ca>
2021-10-21 15:11:10 -0700

    Add constructor, getters, and setters

--- app/src/main/java/ca/cmpt276/titanium/model/Child.java
+++ app/src/main/java/ca/cmpt276/titanium/model/Child.java
@@ -1,4 +1,23 @@
 package ca.cmpt276.titanium.model;
 
 public class Child {
+    private final int uniqueId;
+    private String name;
+
+    public Child(String name, int uniqueId) {
+        this.name = name;
+        this.uniqueId = uniqueId;
+    }
+
+    public int getUniqueId() {
+        return uniqueId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
