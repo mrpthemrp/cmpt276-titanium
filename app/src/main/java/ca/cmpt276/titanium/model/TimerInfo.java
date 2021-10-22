83448bbe4fd837e3dba52195d81e5c11ebc56183
Christopher Torunski <cjt13@sfu.ca>
2021-10-21 19:22:30 -0700

    Add constructor, getters, and setters

--- app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
+++ app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
@@ -1,4 +1,45 @@
 package ca.cmpt276.titanium.model;
 
 public class TimerInfo {
+    private int durationSeconds;
+    private int remainingSeconds;
+    private boolean isRunning = false;
+    private boolean isPaused = false;
+
+    public TimerInfo(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+        this.remainingSeconds = durationSeconds;
+    }
+
+    public int getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public void setDurationSeconds(int durationSeconds) {
+        this.durationSeconds = durationSeconds;
+    }
+
+    public int getRemainingSeconds() {
+        return remainingSeconds;
+    }
+
+    public void setRemainingSeconds(int remainingSeconds) {
+        this.remainingSeconds = remainingSeconds;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(boolean isRunning) {
+        this.isRunning = isRunning;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    public void setPaused(boolean isPaused) {
+        this.isPaused = isPaused;
+    }
 }
