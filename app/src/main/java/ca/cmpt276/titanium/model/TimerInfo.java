b95ea0e750709c88752138e3851c93bc69fec01f
Christopher Torunski <cjt13@sfu.ca>
2021-10-22 01:14:59 -0700

    Add ability to save and load SharedPreferences data

--- app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
+++ app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
@@ -1,45 +1,58 @@
 package ca.cmpt276.titanium.model;
 
-public class TimerInfo {
-    private int durationSeconds;
-    private int remainingSeconds;
-    private boolean running = false;
-    private boolean paused = false;
+import android.content.SharedPreferences;
 
-    public TimerInfo(int durationSeconds) {
-        this.durationSeconds = durationSeconds;
-        this.remainingSeconds = durationSeconds;
+public class TimerInfo {
+    private final SharedPreferences prefs;
+    private final SharedPreferences.Editor prefsEditor;
+
+    public TimerInfo(SharedPreferences prefs) {
+        this.prefs = prefs;
+        this.prefsEditor = prefs.edit();
     }
 
     public int getDurationSeconds() {
-        return durationSeconds;
+        return prefs.getInt("duration_seconds", -1);
     }
 
     public void setDurationSeconds(int durationSeconds) {
-        this.durationSeconds = durationSeconds;
+        prefsEditor.putInt("duration_seconds", durationSeconds);
+        prefsEditor.apply();
     }
 
     public int getRemainingSeconds() {
-        return remainingSeconds;
+        return prefs.getInt("remaining_seconds", -1);
     }
 
     public void setRemainingSeconds(int remainingSeconds) {
-        this.remainingSeconds = remainingSeconds;
+        prefsEditor.putInt("remaining_seconds", remainingSeconds);
+        prefsEditor.apply();
     }
 
     public boolean isRunning() {
-        return running;
+        return prefs.getBoolean("is_running", false);
     }
 
-    public void setRunning(boolean running) {
-        this.running = running;
+    public void setRunning(boolean isRunning) {
+        prefsEditor.putBoolean("is_running", isRunning);
+        prefsEditor.apply();
     }
 
     public boolean isPaused() {
-        return paused;
+        return prefs.getBoolean("is_paused", false);
     }
 
-    public void setPaused(boolean paused) {
-        this.paused = paused;
+    public void setPaused(boolean isPaused) {
+        prefsEditor.putBoolean("is_paused", isPaused);
+        prefsEditor.apply();
+    }
+
+    public int getNextDurationSeconds() {
+        return prefs.getInt("next_duration_seconds", -1);
+    }
+
+    public void setNextDurationSeconds(int nextDurationSeconds) {
+        prefsEditor.putInt("next_duration_seconds", nextDurationSeconds);
+        prefsEditor.apply();
     }
 }
