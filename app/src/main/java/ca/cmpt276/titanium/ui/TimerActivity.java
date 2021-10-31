cb509ea3967359604d4f756737f20f057a05b52f
HarisAhmad16 <88016633+HarisAhmad16@users.noreply.github.com>
2021-10-31 15:34:38 -0700

    Timer works for set times (1,2,3,5,10)

--- app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
+++ app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
@@ -1,5 +1,6 @@
 package ca.cmpt276.titanium.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
@@ -10,10 +11,12 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 import ca.cmpt276.titanium.R;
@@ -22,10 +25,13 @@ public class TimerActivity extends AppCompatActivity {
     private ImageView playPause;
     private Button cancelBtn, oneMinButton, twoMinButton, threeMinButton, fiveMinButton,tenMinbutton;
     private Button setTimeButton;
-    private View userInputTime;
+    private EditText userInputTime;
     private boolean isPause;//get from sharedPreferences?
     private boolean inputIsSet;
-    long durationOfTime;
+    private boolean isTimeRunning;
+    long durationOfTime = 0;
+    private TextView time;
+    private CountDownTimer countDownTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +42,10 @@ public class TimerActivity extends AppCompatActivity {
         setupAttributes();
         setupPlayPause();
         setupCancelBtn();
+        setUpButton();
         //make below a method onc shared preferences is sorted out
         if(!this.inputIsSet){
-            setupInput(); //this method implements text watcher
+            // ?
         }
     }
 
@@ -57,32 +64,94 @@ public class TimerActivity extends AppCompatActivity {
         this.tenMinbutton = findViewById(R.id.tenMin);
         this.userInputTime = findViewById(R.id.editTextNumber);
         this.setTimeButton = findViewById(R.id.setTimeButton);
+        this.time = findViewById(R.id.timer);
+
         this.isPause = false;
         this.inputIsSet = false;
     }
 
     private void setUpButton(){
         oneMinButton.setOnClickListener(view -> {
-            durationOfTime = TimeUnit.MINUTES.toMinutes(1);
+            durationOfTime = 60000;
+            setUpTime();
         });
         twoMinButton.setOnClickListener(view -> {
-            durationOfTime = TimeUnit.MINUTES.toMinutes(2);
+            durationOfTime = 120000;
+            setUpTime();
         });
         threeMinButton.setOnClickListener(view -> {
-            durationOfTime = TimeUnit.MINUTES.toMinutes(3);
+            durationOfTime = 180000;
+            setUpTime();
         });
         fiveMinButton.setOnClickListener(view -> {
-            durationOfTime = TimeUnit.MINUTES.toMinutes(5);
+            durationOfTime = 300000;
+            setUpTime();
         });
         tenMinbutton.setOnClickListener(view -> {
-            durationOfTime = TimeUnit.MINUTES.toMinutes(10);
+            durationOfTime = 600000;
+            setUpTime();
+        });
+
+        setTimeButton.setOnClickListener(view -> {
+            durationOfTime = Integer.parseInt(userInputTime.getText().toString());
+            setUpTime();
         });
     }
 
+    private void startCountDown(){
+        countDownTimer = new CountDownTimer(durationOfTime, 1000) {
 
+            @Override
+            public void onTick(long l) {
+                durationOfTime = l;
+                setUpTime();
+            }
 
-    private void setupInput() {
-        //to do
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+
+        isTimeRunning = true;
+    }
+
+    private void stopTimer(){
+        if(isTimeRunning){
+            countDownTimer.cancel();
+            isTimeRunning = false;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("time", durationOfTime);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        durationOfTime = savedInstanceState.getLong("time");
+        time.setText(String.valueOf(durationOfTime));
+    }
+
+    private void setUpTime(){
+        if(durationOfTime == 0){
+            return;
+        }
+        int min = (int) durationOfTime / 60000;
+        int sec = (int) durationOfTime % 60000 / 1000;
+        String timeDuration = "";
+        if(min < 10){
+            timeDuration += "0";
+        }
+        timeDuration += min + ":";
+        if(sec < 10){
+            timeDuration += "0";
+        }
+        timeDuration += sec;
+        time.setText(timeDuration);
     }
 
     private void setupCancelBtn() {
@@ -102,10 +171,12 @@ public class TimerActivity extends AppCompatActivity {
     private void setPlayPause() {
         if(this.isPause){
             this.playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_pause_24, getTheme()));
+            startCountDown();
             this.isPause =false;
         }
         else{
             this.playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_play_arrow_24, getTheme()));
+            stopTimer();
             this.isPause =true;
         }
     }
