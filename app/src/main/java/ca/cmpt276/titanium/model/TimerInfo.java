0f30502490a32dbb519519eb13b9db3cf0b1a7a0
Christopher Torunski <cjt13@sfu.ca>
2021-11-07 18:49:18 -0800

    Encapsulate timer functions and fix notification bugs

--- app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
+++ app/src/main/java/ca/cmpt276/titanium/model/TimerInfo.java
@@ -4,13 +4,73 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-/**
- * This class stores the timer information.
- */
 public class TimerInfo {
+    private static final int INVALID_MILLISECONDS = -1;
+    private static final boolean DEFAULT_RUNNING = false;
+    private static final boolean DEFAULT_STOPPED = true;
+
+    private static TimerInfo instance;
     private static SharedPreferences prefs;
+    private static SharedPreferences.Editor prefsEditor;
 
     private TimerInfo(Context context) {
         TimerInfo.prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        TimerInfo.prefsEditor = prefs.edit();
+    }
+
+    public static TimerInfo getInstance(Context context) {
+        if (instance == null) {
+            TimerInfo.instance = new TimerInfo(context);
+        }
+
+        return instance;
+    }
+
+    public long getDurationMilliseconds() {
+        return prefs.getLong("duration_milliseconds", INVALID_MILLISECONDS);
+    }
+
+    public void setDurationMilliseconds(long durationMilliseconds) {
+        prefsEditor.putLong("duration_milliseconds", durationMilliseconds);
+        prefsEditor.apply();
+    }
+
+    public long getRemainingMilliseconds() {
+        return prefs.getLong("remaining_milliseconds", INVALID_MILLISECONDS);
+    }
+
+    public void setRemainingMilliseconds(long remainingMilliseconds) {
+        prefsEditor.putLong("remaining_milliseconds", remainingMilliseconds);
+        prefsEditor.apply();
+    }
+
+    public boolean isRunning() {
+        return prefs.getBoolean("is_running", DEFAULT_RUNNING);
+    }
+
+    public void setRunning() {
+        prefsEditor.putBoolean("is_running", true);
+        prefsEditor.putBoolean("is_paused", false);
+        prefsEditor.putBoolean("is_stopped", false);
+        prefsEditor.apply();
+    }
+
+    public void setPaused() {
+        prefsEditor.putBoolean("is_running", false);
+        prefsEditor.putBoolean("is_paused", true);
+        prefsEditor.putBoolean("is_stopped", false);
+        prefsEditor.apply();
+    }
+
+    public boolean isStopped() {
+        return prefs.getBoolean("is_stopped", DEFAULT_STOPPED);
+    }
+
+    public void setStopped() {
+        prefsEditor.putLong("remaining_milliseconds", prefs.getLong("duration_milliseconds", INVALID_MILLISECONDS));
+        prefsEditor.putBoolean("is_running", false);
+        prefsEditor.putBoolean("is_paused", false);
+        prefsEditor.putBoolean("is_stopped", true);
+        prefsEditor.apply();
     }
 }
--- app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
+++ app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
@@ -1,12 +1,10 @@
 package ca.cmpt276.titanium.ui;
 
-import android.annotation.SuppressLint;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.media.AudioAttributes;
 import android.media.MediaPlayer;
@@ -19,43 +17,44 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.res.ResourcesCompat;
 
 import ca.cmpt276.titanium.R;
+import ca.cmpt276.titanium.model.TimerInfo;
 
 import java.util.Locale;
 import java.util.Objects;
 
-/**
- * This activity represents the timer activity.
- * Shows times that can be set, and buttons that start and cancel the countdown.
- */
 public class TimerActivity extends AppCompatActivity {
     private static final int MILLIS_IN_SECOND = 1000;
     private static final int MILLIS_IN_MINUTE = 60000;
     private static final int MILLIS_IN_HOUR = 3600000;
 
-    private ImageView playPause;
-    private Button oneMinButton, twoMinButton, threeMinButton, fiveMinButton, tenMinButton;
-    private Button setTimeButton;
-    private EditText userInputTime;
-    private boolean isTimeRunning, isPaused;
-    long durationOfTime;
-    long durationStartTime;
-    private CountDownTimer countDownTimer;
-    private long endTime;
-
+    private TimerInfo timerInfo;
     public static MediaPlayer timerEndSound;
+
+    private Button oneMinButton;
+    private Button twoMinButton;
+    private Button threeMinButton;
+    private Button fiveMinButton;
+    private Button tenMinButton;
+    private EditText userInputTime;
+    private Button setTimeButton;
+    private ImageView playPause;
+
+    private CountDownTimer countDownTimer;
+
+    //private long startTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
+        setTitle(R.string.timerTitle);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
 
         /*
         Receiving notification click found from https://stackoverflow.com/questions/13822509/android-call-method-on-notification-click/14539858
@@ -65,27 +64,61 @@ public class TimerActivity extends AppCompatActivity {
             if (extras == null) {
                 timerEndSound = MediaPlayer.create(TimerActivity.this, R.raw.timeralarm);
             } else if (extras.getBoolean("isClicked")) {
+                timerEndSound.setLooping(false);
                 timerEndSound.stop();
                 startStopVibrations(TimerActivity.this, "off");
+
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.cancel(52);
             }
         }
 
-        timerEndSound = MediaPlayer.create(TimerActivity.this, R.raw.timeralarm);
+        timerInfo = TimerInfo.getInstance(this);
 
-        setupActionBar();
-        setupAttributes();
-        clickResetButton();
-        customTimeFunctionality();
+        setupButtons();
+
+        if (timerInfo.isRunning()) {
+            startTimer();
+        } else {
+            displayTime();
+        }
     }
 
-    private void setupActionBar() {
-        setTitle(R.string.timerTitle);
+    private void startTimer() {
+        startCountDown(timerInfo.getRemainingMilliseconds());
+        timerInfo.setRunning();
+        //startTime = System.currentTimeMillis();
 
-        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_pause_24, getTheme()));
+        makeInputButtonsVisible(false);
     }
 
-    private void setupAttributes() {
-        this.playPause = findViewById(R.id.timerPlayPauseBtn);
+    private void pauseTimer() {
+        countDownTimer.cancel();
+        timerInfo.setPaused();
+
+        playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_play_arrow_24, getTheme()));
+        makeInputButtonsVisible(true);
+    }
+
+    private void resetTimer() {
+        if (countDownTimer != null) {
+            countDownTimer.cancel();
+        }
+
+        timerInfo.setStopped();
+        displayTime();
+
+        playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_play_arrow_24, getTheme()));
+        makeInputButtonsVisible(true);
+    }
+
+    private void changeTimerDuration(long minutes) {
+        timerInfo.setDurationMilliseconds(minutes * MILLIS_IN_MINUTE);
+        resetTimer();
+    }
+
+    private void setupButtons() {
         this.oneMinButton = findViewById(R.id.oneMin);
         this.twoMinButton = findViewById(R.id.twoMin);
         this.threeMinButton = findViewById(R.id.threeMin);
@@ -93,168 +126,91 @@ public class TimerActivity extends AppCompatActivity {
         this.tenMinButton = findViewById(R.id.tenMin);
         this.userInputTime = findViewById(R.id.editTextNumber);
         this.setTimeButton = findViewById(R.id.setTimeButton);
-    }
+        this.playPause = findViewById(R.id.timerPlayPauseBtn);
 
-    private void customTimeFunctionality() {
-        oneMinButton.setOnClickListener(view -> changeTime(MILLIS_IN_MINUTE, MILLIS_IN_MINUTE));
-        twoMinButton.setOnClickListener(view -> changeTime(2 * MILLIS_IN_MINUTE, 2 * MILLIS_IN_MINUTE));
-        threeMinButton.setOnClickListener(view -> changeTime(3 * MILLIS_IN_MINUTE, 3 * MILLIS_IN_MINUTE));
-        fiveMinButton.setOnClickListener(view -> changeTime(5 * MILLIS_IN_MINUTE, 5 * MILLIS_IN_MINUTE));
-        tenMinButton.setOnClickListener(view -> changeTime(10 * MILLIS_IN_MINUTE, 10 * MILLIS_IN_MINUTE));
+        oneMinButton.setOnClickListener(view -> changeTimerDuration(1));
+        twoMinButton.setOnClickListener(view -> changeTimerDuration(2));
+        threeMinButton.setOnClickListener(view -> changeTimerDuration(3));
+        fiveMinButton.setOnClickListener(view -> changeTimerDuration(5));
+        tenMinButton.setOnClickListener(view -> changeTimerDuration(10));
 
-        Button setTimeButton = findViewById(R.id.setTimeButton);
         setTimeButton.setOnClickListener(view -> {
-            if (userInputTime.getText().toString().isEmpty()) {
-                durationOfTime = 0;
-                durationStartTime = 0;
-                return;
+            if (!userInputTime.getText().toString().isEmpty()) {
+                changeTimerDuration(Long.parseLong(userInputTime.getText().toString()));
             }
-            durationOfTime = Integer.parseInt(userInputTime.getText().toString());
-            durationOfTime *= 60000;
-            durationStartTime = Integer.parseInt(userInputTime.getText().toString());
-            durationStartTime *= 60000;
-            changeTime(durationOfTime, durationStartTime);
+        });
+
+        playPause.setOnClickListener(view -> {
+            if (timerInfo.isRunning()) {
+                pauseTimer();
+            } else {
+                startTimer();
+            }
+        });
+
+        Button resetButton = findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(view -> {
+            if (!timerInfo.isStopped()) {
+                resetTimer();
+            }
         });
     }
 
-    private void changeTime(long dur, long durStart) {
-        durationOfTime = dur;
-        durationStartTime = durStart;
-        displayTime();
-        isPaused = true;
-        setPlayPause();
-    }
-
-    private void startCountDown() {
-        endTime = System.currentTimeMillis() + durationOfTime;
-
-        countDownTimer = new CountDownTimer(durationOfTime, 1000) {
-
+    private void startCountDown(long durationMilliseconds) {
+        countDownTimer = new CountDownTimer(durationMilliseconds, MILLIS_IN_SECOND) {
             @Override
             public void onTick(long l) {
-                durationOfTime = l;
+                timerInfo.setRemainingMilliseconds(l);
                 displayTime();
-                makeScreenLookClean(true);
             }
 
             @Override
-            public void onFinish() {
-                setPlayPause();
+            public void onFinish() { // CHANGE
+                resetTimer();
+                displayTime();
+
                 timerEndSound.setLooping(true);
                 timerEndSound.start();
                 startStopVibrations(TimerActivity.this, "start");
                 notificationOnEndTime();
-                makeScreenLookClean(false);
             }
         }.start();
-
-        isTimeRunning = true;
-    }
-
-    private void stopTimer() {
-        if (isTimeRunning) {
-            if (countDownTimer != null) {
-                countDownTimer.cancel();
-            }
-            isTimeRunning = false;
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putLong("time", durationOfTime);
-        editor.putBoolean("timerRunning", isTimeRunning);
-        editor.putLong("endOfTimer", endTime);
-        editor.putLong("startTime", durationStartTime);
-
-        editor.apply();
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        SharedPreferences prefs = getSharedPreferences("prefs", MODE_PRIVATE);
-
-        durationStartTime = prefs.getLong("startTime", 0);
-        durationOfTime = prefs.getLong("time", durationStartTime);
-        isTimeRunning = prefs.getBoolean("timerRunning", false);
-
-        displayTime();
-        setupPlayPause();
-
-        if (isTimeRunning) {
-            endTime = prefs.getLong("endOfTimer", 0);
-            durationOfTime = endTime - System.currentTimeMillis();
-
-            if (durationOfTime < 0) {
-                durationOfTime = 0;
-                isTimeRunning = true;
-                isPaused = true;
-                displayTime();
-            } else {
-                isTimeRunning = false;
-                isPaused = false;
-                setPlayPause();
-            }
-        }
     }
 
     private void displayTime() {
-        int milliseconds = (int) durationOfTime;
-        int hours = milliseconds / MILLIS_IN_HOUR;
-        int minutes = (milliseconds % MILLIS_IN_HOUR) / MILLIS_IN_MINUTE;
-        int seconds = (milliseconds % MILLIS_IN_MINUTE) / MILLIS_IN_SECOND;
+        long milliseconds = timerInfo.getRemainingMilliseconds();
+        long hours = milliseconds / MILLIS_IN_HOUR;
+        long minutes = (milliseconds % MILLIS_IN_HOUR) / MILLIS_IN_MINUTE;
+        long seconds = (milliseconds % MILLIS_IN_MINUTE) / MILLIS_IN_SECOND;
         String formattedTime = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
 
         TextView time = findViewById(R.id.timer);
         time.setText(formattedTime);
     }
 
-    private void setupPlayPause() {
-        this.playPause.setOnClickListener(view -> setPlayPause());
-    }
+    private void makeInputButtonsVisible(boolean isVisible) {
+        int visibility;
 
-    private void clickResetButton() {
-        Button resetButton = findViewById(R.id.resetButton);
-        resetButton.setOnClickListener(view -> {
-            durationOfTime = durationStartTime;
-            displayTime();
-            isPaused = true;
-            setPlayPause();
-        });
-    }
-
-    private void setPlayPause() {
-        if (durationOfTime == 0) {
-            return;
-        }
-
-        if (this.isTimeRunning || isPaused) {
-            this.playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_play_arrow_24, getTheme()));
-            stopTimer();
-            isPaused = false;
-            makeScreenLookClean(false);
+        if (isVisible) {
+            visibility = View.VISIBLE;
         } else {
-            this.playPause.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_baseline_pause_24, getTheme()));
-            startCountDown();
-            isPaused = true;
-            makeScreenLookClean(true);
+            visibility = View.INVISIBLE;
         }
-    }
 
-    public static void stopSound() {
-        timerEndSound.stop();
+        oneMinButton.setVisibility(visibility);
+        twoMinButton.setVisibility(visibility);
+        threeMinButton.setVisibility(visibility);
+        fiveMinButton.setVisibility(visibility);
+        tenMinButton.setVisibility(visibility);
+        userInputTime.setVisibility(visibility);
+        setTimeButton.setVisibility(visibility);
+        findViewById(R.id.minuteText).setVisibility(visibility);
     }
 
     /*Got vibration to work form this resource:
      * https://stackoverflow.com/questions/60466695/android-vibration-app-doesnt-work-anymore-after-android-10-api-29-update*/
-    public static void startStopVibrations(Context c, String isStartStop) {
-        Vibrator vibrations = (Vibrator) c.getSystemService(Context.VIBRATOR_SERVICE);
+    public static void startStopVibrations(Context context, String isStartStop) {
+        Vibrator vibrations = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
 
         if (isStartStop.equals("start")) {
             long[] pattern = {0, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500, 1000, 500};
@@ -271,7 +227,6 @@ public class TimerActivity extends AppCompatActivity {
     Building a notification found from https://stackoverflow.com/questions/47409256/what-is-notification-channel-idnotifications-not-work-in-api-27
     Using a pending intent found from https://www.youtube.com/watch?v=CZ575BuLBo4
      */
-    @SuppressLint("UnspecifiedImmutableFlag")
     private void notificationOnEndTime() {
         NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
@@ -285,47 +240,21 @@ public class TimerActivity extends AppCompatActivity {
 
         Intent intent = new Intent(getApplicationContext(), TimerActivity.class);
         intent.putExtra("isClicked", true);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        Intent broadcastIntent = new Intent(this, NotificationReceiver.class);
-        broadcastIntent.putExtra("sound", "off");
-        PendingIntent soundIntent = PendingIntent.getBroadcast(this, 0, broadcastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "TIMER")
                 .setSmallIcon(R.drawable.ic_time)
                 .setContentTitle("Time Is Up")
                 .setContentText("Click OK to stop the sound and vibration")
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                .setCategory(NotificationCompat.CATEGORY_ALARM)
                 .setColor(Color.GREEN)
                 .setVibrate(new long[]{0L})
-                .addAction(R.drawable.ic_sound, "OK", soundIntent)
+                .addAction(R.drawable.ic_sound, "OK", pendingIntent)
                 .setAutoCancel(true);
 
         builder.setContentIntent(pendingIntent);
-        manager.notify(0, builder.build());
-    }
-
-    private void makeScreenLookClean(boolean isTimeRunning) {
-        if (isTimeRunning) {
-            oneMinButton.setVisibility(View.INVISIBLE);
-            twoMinButton.setVisibility(View.INVISIBLE);
-            threeMinButton.setVisibility(View.INVISIBLE);
-            fiveMinButton.setVisibility(View.INVISIBLE);
-            tenMinButton.setVisibility(View.INVISIBLE);
-            setTimeButton.setVisibility(View.INVISIBLE);
-            userInputTime.setVisibility(View.INVISIBLE);
-            findViewById(R.id.minuteText).setVisibility(View.INVISIBLE);
-        } else {
-            oneMinButton.setVisibility(View.VISIBLE);
-            twoMinButton.setVisibility(View.VISIBLE);
-            threeMinButton.setVisibility(View.VISIBLE);
-            fiveMinButton.setVisibility(View.VISIBLE);
-            tenMinButton.setVisibility(View.VISIBLE);
-            setTimeButton.setVisibility(View.VISIBLE);
-            userInputTime.setVisibility(View.VISIBLE);
-            findViewById(R.id.minuteText).setVisibility(View.VISIBLE);
-        }
+        manager.notify(52, builder.build());
     }
 
     public static Intent makeIntent(Context context) {
