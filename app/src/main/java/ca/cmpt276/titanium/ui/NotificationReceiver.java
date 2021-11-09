861dfa02226ba1157e34733e115c5b0b7d50fd7f
Christopher Torunski <cjt13@sfu.ca>
2021-11-08 20:31:16 -0800

    Remove print statements

--- app/src/main/java/ca/cmpt276/titanium/ui/NotificationReceiver.java
+++ app/src/main/java/ca/cmpt276/titanium/ui/NotificationReceiver.java
@@ -9,9 +9,7 @@ public class NotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         boolean dismissed = intent.getBooleanExtra("dismissed", false);
 
-        System.out.println("dismissed");
         if (dismissed) {
-            System.out.println("dismissed2");
             TimerActivity.dismissNotification(context.getApplicationContext());
             TimerActivity.toggleVibrations(context.getApplicationContext(), false);
         }
