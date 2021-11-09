2b58faff0718fcda1fb48978f5b77ffa2493b55a
Christopher Torunski <cjt13@sfu.ca>
2021-11-08 23:36:52 -0800

    Add toast if switching activities from timer while timer is active

--- app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
+++ app/src/main/java/ca/cmpt276/titanium/ui/TimerActivity.java
@@ -17,6 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -98,6 +99,7 @@ public class TimerActivity extends AppCompatActivity {
 
         if (timerInfo.isRunning()) {
             pauseTimer();
+            Toast.makeText(this, "Timer paused", Toast.LENGTH_SHORT).show();
         }
     }
 
