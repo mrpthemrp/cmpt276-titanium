f3f1d74f530f7d92022d041892e9037ef5514e31
Christopher Torunski <cjt13@sfu.ca>
2021-11-18 17:19:37 -0800

    Related to last commit

--- app/src/main/java/ca/cmpt276/titanium/ui/CoinFlipHistoryActivity.java
+++ app/src/main/java/ca/cmpt276/titanium/ui/CoinFlipHistoryActivity.java
@@ -7,11 +7,9 @@ import android.widget.ListView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import java.util.ArrayList;
 import java.util.Collections;
 
 import ca.cmpt276.titanium.R;
-import ca.cmpt276.titanium.model.CoinFlip;
 import ca.cmpt276.titanium.model.CoinFlipHistory;
 
 /**
@@ -19,15 +17,18 @@ import ca.cmpt276.titanium.model.CoinFlipHistory;
  * Shows a list of the history from latest to oldest.
  */
 public class CoinFlipHistoryActivity extends AppCompatActivity {
+    private CoinFlipHistory coinFlipHistory;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coin_flip_history);
         setTitle(R.string.coinFlipHistoryTitle);
 
+        this.coinFlipHistory = CoinFlipHistory.getInstance(this);
         populateListView();
 
-        if (CoinFlipHistory.getCoinFlipHistory().size() == 0) {
+        if (coinFlipHistory.getCoinFlipHistory().size() == 0) {
             ConstraintLayout emptyStateLayout = findViewById(R.id.emptyStateLayout);
             emptyStateLayout.setVisibility(View.VISIBLE);
 
@@ -37,9 +38,8 @@ public class CoinFlipHistoryActivity extends AppCompatActivity {
     }
 
     private void populateListView() {
-        ArrayList<CoinFlip> coinFlipHistory = CoinFlipHistory.getCoinFlipHistory();
-        Collections.reverse(coinFlipHistory);
-        CoinFlipHistoryAdapter adapter = new CoinFlipHistoryAdapter(this, coinFlipHistory);
+        Collections.reverse(coinFlipHistory.getCoinFlipHistory());
+        CoinFlipHistoryAdapter adapter = new CoinFlipHistoryAdapter(this, coinFlipHistory.getCoinFlipHistory());
         ListView list = findViewById(R.id.coinFlipHistoryList);
         list.setAdapter(adapter);
     }
