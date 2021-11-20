1a113480867cb4013cb02da8a7a785a95f358da7
HarisAhmad16 <88016633+HarisAhmad16@users.noreply.github.com>
2021-11-19 23:12:15 -0800

    Task model updated

--- app/src/main/java/ca/cmpt276/titanium/model/Tasks.java
+++ app/src/main/java/ca/cmpt276/titanium/model/Tasks.java
@@ -1,4 +1,23 @@
 package ca.cmpt276.titanium.model;
 
+import java.util.ArrayList;
+
 public class Tasks {
+    private final ArrayList<String> listOfTasks = new ArrayList<>();
+
+    public void addTask(String task){
+        listOfTasks.add(task);
+    }
+
+    public void removeTask(int index){
+        listOfTasks.remove(index);
+    }
+
+    public void editTask(int index, String newTask){
+        listOfTasks.set(index, newTask);
+    }
+
+    public String getTask(int index){
+        return listOfTasks.get(index);
+    }
 }
