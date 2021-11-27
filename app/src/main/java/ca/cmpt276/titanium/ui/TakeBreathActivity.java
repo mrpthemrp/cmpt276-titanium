0bbf06fa23f129b8d2b05f5380dc5e36fa66d8e8
Deborah Wang <dqw@sfu.ca>
2021-11-26 17:23:03 -0800

    TakeBreath Empty Activity and Blank XML made.

---
+++ app/src/main/java/ca/cmpt276/titanium/ui/TakeBreathActivity.java
@@ -0,0 +1,16 @@
+package ca.cmpt276.titanium.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ca.cmpt276.titanium.R;
+
+public class TakeBreathActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_breath);
+    }
+}
