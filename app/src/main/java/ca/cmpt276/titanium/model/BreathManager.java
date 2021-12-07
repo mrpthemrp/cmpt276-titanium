8acf5f0861e15f6686098b92c755eaea77c6635e
mva30 <mva30@sfu.ca>
2021-12-06 18:42:20 -0800

    Fix indentation.

--- app/src/main/java/ca/cmpt276/titanium/model/BreathManager.java
+++ app/src/main/java/ca/cmpt276/titanium/model/BreathManager.java
@@ -16,53 +16,53 @@ import java.lang.reflect.Type;
  * @author Titanium
  */
 public class BreathManager {
-    private static final Gson GSON = new Gson();
-    private static final String BREATH_JSON_KEY = "breathJson";
+  private static final Gson GSON = new Gson();
+  private static final String BREATH_JSON_KEY = "breathJson";
 
-    private static BreathManager instance;
-    private static SharedPreferences prefs;
+  private static BreathManager instance;
+  private static SharedPreferences prefs;
 
-    private static int numBreaths;
+  private static int numBreaths;
 
-    private BreathManager(Context context) {
-        BreathManager.prefs = PreferenceManager.getDefaultSharedPreferences(context);
+  private BreathManager(Context context) {
+    BreathManager.prefs = PreferenceManager.getDefaultSharedPreferences(context);
+  }
+
+  public static BreathManager getInstance(Context context) {
+    if (instance == null) {
+      BreathManager.instance = new BreathManager(context);
     }
 
-    public static BreathManager getInstance(Context context) {
-        if (instance == null) {
-            BreathManager.instance = new BreathManager(context);
-        }
+    loadSavedData();
+    return instance;
+  }
 
-        loadSavedData();
-        return instance;
+  public static void loadSavedData() {
+    String breathJson = prefs.getString(BREATH_JSON_KEY, null);
+
+    if (breathJson != null) {
+      Type breathType = new TypeToken<Integer>() {
+      }.getType();
+
+      BreathManager.numBreaths = GSON.fromJson(breathJson, breathType);
     }
+  }
 
-    public static void loadSavedData() {
-        String breathJson = prefs.getString(BREATH_JSON_KEY, null);
+  public void saveData() {
+    String breathJson = GSON.toJson(numBreaths);
 
-        if (breathJson != null) {
-            Type breathType = new TypeToken<Integer>() {
-            }.getType();
+    SharedPreferences.Editor prefsEditor = prefs.edit();
+    prefsEditor.putString(BREATH_JSON_KEY, breathJson);
+    prefsEditor.apply();
+  }
 
-            BreathManager.numBreaths = GSON.fromJson(breathJson, breathType);
-        }
-    }
+  public int getNumBreaths() {
+    return numBreaths;
+  }
 
-    public void saveData() {
-        String breathJson = GSON.toJson(numBreaths);
-
-        SharedPreferences.Editor prefsEditor = prefs.edit();
-        prefsEditor.putString(BREATH_JSON_KEY, breathJson);
-        prefsEditor.apply();
-    }
-
-    public int getNumBreaths() {
-        return numBreaths;
-    }
-
-    public static void setNumBreaths(int numBreaths) {
-        BreathManager.numBreaths = numBreaths;
-    }
+  public static void setNumBreaths(int numBreaths) {
+    BreathManager.numBreaths = numBreaths;
+  }
 }
 
 
